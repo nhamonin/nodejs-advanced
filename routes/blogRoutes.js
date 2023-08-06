aad14e9967dc91c85240eb52bcf5f6810aca38b0
nhamonin <nazarii.hamonin@gmail.com>
2023-08-06 23:58:04 +0300

    Add draft caching for blogs API endpoint via Redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,18 +3,34 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const util = require('util');
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    console.log('SERVING FROM MONGODB');
     const blogs = await Blog.find({ _user: req.user.id });
+
+    client.set(req.user.id, JSON.stringify(blogs));
 
     res.send(blogs);
   });
@@ -25,7 +41,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
