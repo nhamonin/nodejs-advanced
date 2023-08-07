c38ba3ac01e8c09c70a1cc0fe9d20a66bf869405
nhamonin <nazarii.hamonin@gmail.com>
2023-08-07 22:28:20 +0300

    Restore blog routes handler

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,23 +14,7 @@ module.exports = (app) => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const util = require('util');
-    const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-    client.get = util.promisify(client.get);
-
-    const cachedBlogs = await client.get(req.user.id);
-
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHE');
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    console.log('SERVING FROM MONGODB');
     const blogs = await Blog.find({ _user: req.user.id });
-
-    client.set(req.user.id, JSON.stringify(blogs));
 
     res.send(blogs);
   });
