c641cf6dcf34c74fdf10c704a3ef516bb33703af
nhamonin <nazarii.hamonin@gmail.com>
2023-08-12 16:04:47 +0300

    Switch to the headless mode in order to run tests quickly & minor improvements

--- tests/header.test.js
+++ tests/header.test.js
@@ -4,20 +4,18 @@ test('We can launch a browser', async () => {
   let browser;
   try {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: 'new',
     });
     const page = await browser.newPage();
-
-    await page.goto('localhost:3000');
+    await page.goto('http://localhost:3000');
 
     const logoElement = await page.$('a.brand-logo');
     if (!logoElement) throw new Error('Element a.brand-logo not found');
 
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
     expect(text).toEqual('Blogster');
   } catch (error) {
     console.error('Error during test:', error);
-    throw error;
   } finally {
     if (browser) await browser.close();
   }
