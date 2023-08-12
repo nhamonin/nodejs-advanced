b9ce4c67416d6164e38bc2703f61c41ffdbfd07f
nhamonin <nazarii.hamonin@gmail.com>
2023-08-12 16:29:08 +0300

    Add one more test & tests optimization

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,15 +2,18 @@ const puppeteer = require('puppeteer');
 
 let browser, page;
 
-beforeEach(async () => {
+beforeAll(async () => {
   browser = await puppeteer.launch({
     headless: 'new',
   });
   page = await browser.newPage();
+});
+
+beforeEach(async () => {
   await page.goto('http://localhost:3000');
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await browser.close();
 });
 
@@ -18,4 +21,12 @@ test('Header logo has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
