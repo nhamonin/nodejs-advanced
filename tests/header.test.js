8dadf1bfa6c91df194fea5c8ef574e7475b72468
nhamonin <nazarii.hamonin@gmail.com>
2023-08-12 16:09:39 +0300

    Refactor browser launch and close in header.test.js

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,23 @@
 const puppeteer = require('puppeteer');
 
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: 'new',
+  });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
 test('We can launch a browser', async () => {
-  let browser;
-  try {
-    browser = await puppeteer.launch({
-      headless: 'new',
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
+  const logoElement = await page.$('a.brand-logo');
+  if (!logoElement) throw new Error('Element a.brand-logo not found');
 
-    const logoElement = await page.$('a.brand-logo');
-    if (!logoElement) throw new Error('Element a.brand-logo not found');
-
-    const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-    expect(text).toEqual('Blogster');
-  } catch (error) {
-    console.error('Error during test:', error);
-  } finally {
-    if (browser) await browser.close();
-  }
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
