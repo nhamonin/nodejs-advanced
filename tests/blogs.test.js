f5866a2c78a87b057bfc598079e87b078e1a65b4
nhamonin <nazarii.hamonin@gmail.com>
2023-08-17 12:24:55 +0300

    Add tests for submitting valid and invalid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,4 +22,43 @@ describe('When logged in', () => {
 
     expect(label).toEqual('Blog Title');
   });
+
+  describe('And using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
+    });
+
+    test('Submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('Submitting then saving adds blog to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
+    });
+  });
+
+  describe('And using invalid inputs', () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('The form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
